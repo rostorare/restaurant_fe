a38f6f1d1799b9167dad794c6528bf4c1bdb8c32
s0568127 <mwangi.zwyssig@student.htw-berlin.de>
2022-12-08 18:33:38 +0100

    ci file

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+    <!-- Navbar content -->
+
+  <ul class="nav nav-pills">
+    <li class="nav-item">
+      <router-link class="nav-link" to="/">Home</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link class="nav-link" to="/gerichte">Menu</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link class="nav-link" to="/about">About</router-link>
+    </li>
+  </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav-bar'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/MenuView.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>gerichte </h1>
+</template>
+
+<script>
+export default {
+  name: 'MenuView.vue'
+}
+</script>
+
+<style scoped>
+
+</style>
