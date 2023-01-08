c8085f8ba68d4232dde5552635b9d354774d5a23
s0568127 <mwangi.zwyssig@student.htw-berlin.de>
2023-01-08 12:11:19 +0100

    test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,22 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import GerichtPostPutDeleteComponent from '@/components/GerichtPostPutDeleteComponent.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('GerichtPostPutDeleteComponent', () => {
+  it('submits the form', () => {
+    // Arrange
+    const wrapper = shallowMount(GerichtPostPutDeleteComponent)
+    const gericht = {
+      id: '1',
+      name: 'Test Gericht',
+      beschreibung: 'This is a test gericht',
+      preis: '9.99'
+    }
+    wrapper.setData({ gericht })
+
+    // Act
+    wrapper.find('form').trigger('submit.prevent')
+
+    // Assert
+    expect(wrapper.vm.gericht).to.deep.equal(gericht)
   })
 })
